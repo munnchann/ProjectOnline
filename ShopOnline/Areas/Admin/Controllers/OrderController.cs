523c8d91b9be175d5174362e97347e1937ae0734
thuyvie <thuyvy1235789@gmail.com>
2022-04-18 16:38:28 +0700

    Upcode
    View UIAdmin

---
+++ ShopOnline/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Admin/Order
+        public ActionResult ViewOrd()
+        {
+            return View();
+        }
+
+        public ActionResult Ord()
+        {
+            return View();
+        }
+
+        
+    }
+
+}
---
+++ ShopOnline/Areas/Admin/Controllers/StaffController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Areas.Admin.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Admin/Staff
+        public ActionResult ViewStaff()
+        {
+            return View();
+        }
+
+        public ActionResult EditStaff()
+        {
+            return View();
+        }
+
+        public ActionResult AdStaff()
+        {
+            return View();
+        }
+
+    }
+}
